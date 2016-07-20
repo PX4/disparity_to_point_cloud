40a5c662eda573043a4191e9b47006a0e56dbdd5
Simone Guscetti <simonegu@student.ethz.ch>
2016-07-20 17:28:42 +0200

    OpenCV disparity computation

--- include/disparity_to_point_cloud/disparity_to_point_cloud.hpp
+++ include/disparity_to_point_cloud/disparity_to_point_cloud.hpp
@@ -5,6 +5,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <cv_bridge/cv_bridge.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
@@ -21,13 +22,14 @@ class Disparity2PCloud {
   ros::Publisher p_cloud_pub_;
   ros::Subscriber disparity_sub_;
   // TODO import this coefficeint with the calibration file or camera info topic
-  float fx_ = 704;
-  float fy_ = 704;
+  float fx_ = 707.05;
+  float fy_ = 707.05;
   float cx_ = 320;
   float cy_ = 240;
   float base_line_ = 0.2;
   int min_count_ = 13;
   int threshold_ = 1;
+  cv::Mat Q_;
 
  public:
   Disparity2PCloud() : nh_("~") {
@@ -42,6 +44,17 @@ class Disparity2PCloud {
     if (!nh_.getParam("disparity_threshold", threshold_)) {
       ROS_WARN("Failed to load parameter disparity_threshold");
     }
+    cv::Mat K = (cv::Mat_<double>(3, 3) << fx_, 0, cx_, 0, fy_, cy_, 0, 0, 1);
+    cv::Mat distCoeff1 = (cv::Mat_<double>(5, 1) << 0.0, 0.0, 0.0, 0.0, 0.0);
+    cv::Mat R = cv::Mat::eye(3, 3, CV_64FC1);
+    cv::Mat t = (cv::Mat_<double>(3, 1) << -base_line_, 0, 0);
+
+    cv::Mat R1, R2, P1, P2, Q;
+    cv::Size s;
+    s.height = 480;
+    s.width = 640;
+    cv::stereoRectify(K, distCoeff1, K, distCoeff1, s, R, t, R1, R2, P1, P2,
+                      Q_);
   };
   // virtual ~Disparity2PCloud();
   void DisparityCb(const sensor_msgs::ImageConstPtr &msg);
